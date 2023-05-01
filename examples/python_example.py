661453ec5e1e6be811678b38e1dbbae87059a724
tsunyoku <mbruhyo@gmail.com>
2023-05-01 18:06:11 +0100

    feat: add python example

---
+++ examples/python_example.py
@@ -0,0 +1,22 @@
+from ctypes import CDLL, c_char_p
+
+tools_dll = CDLL("./osz2Tools")
+decrypt_osz2_func = tools_dll.decrypt_osz2
+decrypt_patch_func = tools_dll.decrypt_patch
+
+def decrypt_osz2(osz2_path: str, target_path: str) -> None:
+    osz2_ptr = c_char_p(osz2_path.encode())
+    target_ptr = c_char_p(target_path.encode())
+    
+    decrypt_osz2_func(osz2_ptr, target_ptr)
+    
+def decrypt_patch(patch_path: str, old_osz2_path: str, target_path: str) -> None:
+    patch_ptr = c_char_p(patch_path.encode())
+    old_osz2_ptr = c_char_p(old_osz2_path.encode())
+    target_ptr = c_char_p(target_path.encode())
+    
+    decrypt_patch_func(patch_ptr, old_osz2_ptr, target_ptr)
+
+if __name__ == "__main__":
+    decrypt_osz2("test.osz2", "output")
+    decrypt_patch("test.patch", "test.osz2", "output")
